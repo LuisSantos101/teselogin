fcf6822cf4bbe9b203d476a61d7e60b309f4bac2
LuisSantos101 <luis.santos@ticto.com.br>
2022-08-01 08:25:08 -0300

    Refactor Code

--- cypress/integration/cadastro.spec.js
+++ cypress/integration/cadastro.spec.js
@@ -1,14 +1,39 @@
 /// <reference types="cypress" />
-import type from '../support/pages'
+
+import "../support/qateste"
 
 describe('Cadastrar Novo Usuário', () => {
 
-    beforeEach(() => {
-        cy.visit(Cypress.env('url'));
-    });
+   beforeEach(() => {
+      cy.visit(Cypress.env('url'));
+   });
 
-    it('realizar cadastro', () => {
-       type.criarCadastro()
-    });
+   it('Cadastrar com Nome Inválido', () => {
+      cy.nomeInvalido ()
+   });
+
+   it('Cadastrar com E-mail Inválido', () => {
+      cy.emailInvalido()
+   });
+
+   it('Cadastrar com Senha Inválida', () => {
+      cy.senhaInvalida()
+   });
+   
+   it('Realizar Cadastro Válido', () => {
+       cy.criarCadastro()
+   });
+
+   it('Edição de Cadastro Inválido', () => {
+      cy.editarCadastroInvalido()              
+   });
+
+   it('Edição de Cadastro Válida', () => {
+      cy.editarCadastro()               
+   });
+
+   it('Excluir um Cadastro', () => {
+      cy.excluirCadastro()               
+   });
 });
 
--- cypress/support/pages/elements.js
+++
@@ -1,9 +0,0 @@
-export const ELEMENTS = {
-
-    inserirNome: '#name',
-    inserirEmail: '#email',
-    inserirSenha: '#password',
-    btnCadastrar: 'form >.btn',
-    campoAlert: '.alert',
-    btnSalvar: '.modal-footer > .btn-primary'
-} 
